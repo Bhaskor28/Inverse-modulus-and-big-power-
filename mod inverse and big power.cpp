01472e0e0d2197375989fa38ac4356d8818afcf0
Bhaskor28 <112170792+Bhaskor28@users.noreply.github.com>
2023-07-22 22:10:48 +0600

    Add files via upload

---
+++ mod inverse and big power.cpp
@@ -0,0 +1,42 @@
+
+// Bhaskor Roy
+#include<bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+#define       forn(i,n)              for(int i=0;i<n;i++)
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+const int MOD=1000000007;
+long long int inverse(long long int i){
+    if(i==1) return 1;
+    return (MOD - ((MOD/i)*inverse(MOD%i))%MOD+MOD)%MOD;
+}
+
+
+ll POW(ll a,ll b)
+{
+    if(b==0) return 1;
+    if(b==1) return a%MOD;
+    ll temp=POW(a,b/2);
+    if(b%2==0) return (temp*temp)%MOD;
+    else return (((temp*temp)%MOD)*a)%MOD;
+}
+
+int main(){
+
+cout<<pow(2,100)<<endl;
+
+
+
+return 0;
+}
+
+
